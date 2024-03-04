bb372ba92681af01b783f7a582f2d6a019914fc2
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-03-03 20:41:49 -0500

    add selenium

--- index.py
+++ index.py
@@ -1,2 +1,17 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+website = 'https://medicalfuturist.com/magazine'
+driver.get(website)
+
+url = 'https://medicalfuturist.com/magazine'
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+articles = soup.find_all('section', id='main-section')
+#for article in articles:
+#    article_list = article.find('div', class_='row post-list')
+#    content = article_list.find('div', class_='col-md-3 item ')
+#    print(content)
