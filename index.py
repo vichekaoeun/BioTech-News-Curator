3e6fe92aafea4e973ce45e43df2390f052ef4002
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-03-05 00:51:13 -0500

    implement pharsing and filtering feature

--- index.py
+++ index.py
@@ -18,11 +18,16 @@ soup = BeautifulSoup(html_content, 'html.parser')
 
 articles = soup.find('div', class_='row post-list').find_all('div', class_='col-md-3 item')
 
-for article in range(1):
-    title = articles[article].find('h3', class_='title no-border')
-    link = articles[article].find('a')['href']
-    print("Title: " + title.text)
-    print("Link: " + link)
+keyword_filter = input('Filter by keyword: ')
+print('Filtering out... ' + keyword_filter + '\n')
+
+for article in articles:
+    title = article.find('h3', class_='title no-border').text
+    link = article.find('a')['href']
+    if keyword_filter.lower() in title.lower():
+        print("Title: " + title)
+        print("Link: " + link)
+        print("\n")
 
 # Close the Selenium webdriver
 driver.quit()
