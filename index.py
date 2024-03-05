c055f1078ab472cf9ea3324bb98a47a928c77014
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-03-05 01:13:11 -0500

    add timer & save to txt file

--- index.py
+++ index.py
@@ -3,31 +3,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import time
 
 driver = webdriver.Chrome()
-
 url = 'https://medicalfuturist.com/magazine'
 driver.get(url)
 
-# Wait for the content to load (you may need to adjust the timeout)
-wait = WebDriverWait(driver, 10)
-wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'row.post-list')))
+def get_article():
+    try:
+        # Wait for the content to load (you may need to adjust the timeout)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'row.post-list')))
 
-html_content = driver.page_source
-soup = BeautifulSoup(html_content, 'html.parser')
+        html_content = driver.page_source
+        soup = BeautifulSoup(html_content, 'html.parser')
 
-articles = soup.find('div', class_='row post-list').find_all('div', class_='col-md-3 item')
+        articles = soup.find('div', class_='row post-list').find_all('div', class_='col-md-3 item')
 
-keyword_filter = input('Filter by keyword: ')
-print('Filtering out... ' + keyword_filter + '\n')
+        keyword_filter = input('Filter by keyword: ')
+        print('Filtering out... ' + keyword_filter + '\n')
 
-for article in articles:
-    title = article.find('h3', class_='title no-border').text
-    link = article.find('a')['href']
-    if keyword_filter.lower() in title.lower():
-        print("Title: " + title)
-        print("Link: " + link)
-        print("\n")
+        for index, article in enumerate(articles):
+            title = article.find('h3', class_='title no-border').text
+            link = article.find('a')['href']
+            if keyword_filter.lower() in title.lower():
+                with open(f'scrapped_articles/{index}.txt', 'w') as f:
+                    f.write("Title: " + title + "\n")
+                    f.write("Link: " + link + "\n")
+                print(f'File saved: {index}')
+    finally:
+        # Close the Selenium webdriver
+        driver.quit()
 
-# Close the Selenium webdriver
-driver.quit()
+if __name__ == '__main__':
+    while True:
+        get_article()
+        time_wait = 1
+        print(f"Waiting...{time_wait} minutes")
+        time.sleep(time_wait * 60)
