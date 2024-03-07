7590662011dfab99e56d3b562c218e0382c34543
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-03-07 10:29:33 -0500

    fix cleaning bugs and driver issues

--- index.py
+++ index.py
@@ -5,10 +5,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 
-driver = webdriver.Chrome()
-
 def get_article():
     try:
+        driver = webdriver.Chrome()
         url = 'https://medicalfuturist.com/magazine'
         driver.get(url)
         
@@ -21,7 +20,7 @@ def get_article():
 
         articles = soup.find('div', class_='row post-list').find_all('div', class_='col-md-3 item')
 
-        keyword_filter = input('Filter by keyword: ')
+        keyword_filter = input('Filter by keyword (MedicalFuturist): ')
         print('Filtering out... ' + keyword_filter + '\n')
 
         for index, article in enumerate(articles):
@@ -38,29 +37,33 @@ def get_article():
         
 
 def get_article_medEU():
-    url = 'https://www.medtecheurope.org/news-and-events/news/'
-    driver.get(url)
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.posts__list.is-loaded')))
-    
-    html_content = driver.page_source
-    soup = BeautifulSoup(html_content, 'html.parser')
-    articles = soup.find_all('div', class_='posts__list-item')
-    
-    for article in articles:
-        title = article.find('h2').text
-        link = article.find('a')['href']
-        date = article.find('p').text
-        print(title)
-        print(link)
-        print(date)
+    try: 
+        driver = webdriver.Chrome()
+        url = 'https://www.medtecheurope.org/news-and-events/news/'
+        driver.get(url)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.posts__list.is-loaded')))
+        
+        html_content = driver.page_source
+        soup = BeautifulSoup(html_content, 'html.parser')
+        articles = soup.find_all('div', class_='posts__list-item')
+        
+        for index, article in enumerate(articles):
+            title = article.find('h2').text
+            link = article.find('a')['href']
+            date = article.find('p').text
+            with open(f'scrapped_articles/{index}.txt', 'w') as f:
+                f.write("Title: " + title + "\n")
+                f.write("Link: " + link + "\n")
+                f.write("Date: " + date + "\n")
+    finally:
+        driver.quit()
 
 
-#if __name__ == '__main__':
-#    while True:
-#        get_article_medEU()
-#        time_wait = 1
-#        print(f"Waiting...{time_wait} minutes")
-#        time.sleep(time_wait * 60)
-
-get_article_medEU()
+if __name__ == '__main__':
+    while True:
+        get_article()
+        get_article_medEU()
+        time_wait = 1
+        print(f"Waiting...{time_wait} minutes")
+        time.sleep(time_wait * 60)
